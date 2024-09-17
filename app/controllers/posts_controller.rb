146fa87ec2cf0ed357a391778ad559b88ad31165
Vinay@itworks <vinay.poul@infotech.works>
2024-09-17 19:00:31 +0530

    Added paginate feature too the views.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,9 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    # @posts = Post.all
+    # Using kaminari
+    @posts = Post.page(params[:page]).per(5)
   end
 
   # GET /posts/1 or /posts/1.json
